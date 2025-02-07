a89bcd59968f7670f74fe0e1849580dc31292455
Dorian Maliszewski <maliszewskid3@gmail.com>
2025-02-07 10:05:59 +0100

    chore: ok

--- src/utils/getQuiz.ts
+++ src/utils/getQuiz.ts
@@ -2,46 +2,53 @@ import type { APIResponse } from "types";
 import { questions } from "questions";
 
 // Yolo cache but it works
-let cachedData: { [x: string]: {res: APIResponse, lastFetch: number} } = {};
-const CACHE_DURATION = import.meta.env.DEV ? 60 * 60 * 8 * 1000 :  60 * 1000; // *hours in dev otherwise 1 minute
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxsUnyUPjQhv1mjnRLPcIN8Z5W3vjzG1AWvVhcQOEmENHUkkB2mb77oL31pXSW8Q-6J/exec"
-const API_KEY = import.meta.env.API_KEY
+let cachedData: { [x: string]: { res: APIResponse; lastFetch: number } } = {};
+const CACHE_DURATION = import.meta.env.DEV ? 60 * 60 * 8 * 1000 : 60 * 1000; // *hours in dev otherwise 1 minute
+const SCRIPT_URL =
+	"https://script.google.com/macros/s/AKfycbxsUnyUPjQhv1mjnRLPcIN8Z5W3vjzG1AWvVhcQOEmENHUkkB2mb77oL31pXSW8Q-6J/exec";
+const API_KEY = import.meta.env.API_KEY;
 
 export async function getQuiz(
 	quizId: string,
 ): Promise<APIResponse | undefined> {
-  // Delete me, use me for testing purpose
-  return questions 
+	if (import.meta.env.DEV) {
+		return questions as APIResponse;
+	}
 
-  const now = Date.now();
+	const now = Date.now();
 
-
-	if (cachedData && cachedData[quizId]?.lastFetch && now - cachedData[quizId].lastFetch < CACHE_DURATION) {
+	if (
+		cachedData &&
+		cachedData[quizId]?.lastFetch &&
+		now - cachedData[quizId].lastFetch < CACHE_DURATION
+	) {
 		return cachedData[quizId].res;
 	}
 
-  cleanCache()
+	cleanCache();
 
-  const fullURL = `${SCRIPT_URL}?${new URLSearchParams({key: API_KEY, name: quizId}).toString()}` 
+	const fullURL = `${SCRIPT_URL}?${new URLSearchParams({ key: API_KEY, name: quizId }).toString()}`;
 	const response = await fetch(fullURL);
-  const json = await response.json() as APIResponse
+	const json = (await response.json()) as APIResponse;
 
-	cachedData[quizId] = {res: json, lastFetch: now};
+	cachedData[quizId] = { res: json, lastFetch: now };
 
-
-  return cachedData[quizId].res
+	return cachedData[quizId].res;
 }
 
 function cleanCache() {
-  const now = Date.now()
-  cachedData = Object.entries(cachedData).reduce<typeof cachedData>((acc, [quizId, resAndLastFetch]) => {
-    if (now - resAndLastFetch.lastFetch < CACHE_DURATION) {
-      return {
-        ...acc,
-        [quizId]: resAndLastFetch
-      }
-    }
+	const now = Date.now();
+	cachedData = Object.entries(cachedData).reduce<typeof cachedData>(
+		(acc, [quizId, resAndLastFetch]) => {
+			if (now - resAndLastFetch.lastFetch < CACHE_DURATION) {
+				return {
+					...acc,
+					[quizId]: resAndLastFetch,
+				};
+			}
 
-    return acc
-  }, {})
+			return acc;
+		},
+		{},
+	);
 }
